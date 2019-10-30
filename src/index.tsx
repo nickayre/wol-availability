956a66c36db88464d3e045cf224b2bfe09e2f714
Andrew Short <andrewjshort@gmail.com>
2019-10-30 16:50:50 +1100

    Remove existing content

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
