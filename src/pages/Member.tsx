9ff881892d4e1b1435085e874475f36c359c3aa2
Andrew Short <andrewjshort@gmail.com>
2019-11-12 18:19:21 +1100

    Fab

--- src/pages/Member.tsx
+++ src/pages/Member.tsx
@@ -1,15 +1,28 @@
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
+import Fab from '@material-ui/core/Fab';
 import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import AddIcon from '@material-ui/icons/Add';
 import MenuIcon from '@material-ui/icons/Menu';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import React from 'react';
 
+const useStyles = makeStyles(theme => ({
+  fab: {
+    bottom: theme.spacing(2),
+    position: 'absolute',
+    right: theme.spacing(2),
+  },
+}));
+
 const Member: React.FC = () => {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <AppBar position="static" elevation={0}>
@@ -37,6 +50,9 @@ const Member: React.FC = () => {
         </Typography>
       </Toolbar>
       <Divider />
+      <Fab color="secondary" className={classes.fab}>
+        <AddIcon />
+      </Fab>
     </React.Fragment>
   );
 };
