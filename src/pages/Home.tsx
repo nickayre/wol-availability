a9aa584a01a9b12dbf6c6a48e5776a11e8522ce4
Andrew Short <andrewjshort@gmail.com>
2019-11-14 17:21:12 +1100

    Begin home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,29 @@
 import Page from '../components/Page';
 
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Container from '@material-ui/core/Container';
 import React from 'react';
 
+const StormCard: React.FC = () => (
+  <Card>
+    <CardHeader title="Storm and Support" />
+  </Card>
+);
+
+const RescueCard: React.FC = () => (
+  <Card>
+    <CardHeader title="Rescue" />
+  </Card>
+);
+
 const Home: React.FC = () => (
-  <Page title="Home"></Page>
+  <Page title="Home">
+    <Container>
+      <StormCard />
+      <RescueCard />
+    </Container>
+  </Page>
 );
 
 export default Home;
