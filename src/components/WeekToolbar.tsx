843f8413a0246c6ebf7208f5ca2708a0eb1e2521
Andrew Short <andrewjshort@gmail.com>
2019-11-14 11:09:45 +1100

    Extract week toolbar

---
+++ src/components/WeekToolbar.tsx
@@ -0,0 +1,46 @@
+import { getWeekInterval } from '../model/dates';
+
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { DateTime } from 'luxon';
+import React from 'react';
+
+interface WeekToolbarProps {
+  value: DateTime;
+  onChange: (value: DateTime) => void;
+}
+
+/**
+ * A toolbar which allows selecting a week to view.
+ */
+const WeekToolbar: React.FC<WeekToolbarProps> = ({ value, onChange }) => {
+  // Get the interval for the current week.
+  const { start } = getWeekInterval(value);
+
+  const handleCurrent = () => onChange(DateTime.local());
+  const handlePrevious = () => onChange(start.minus({ week: 1 }));
+  const handleNext = () => onChange(start.plus({ week: 1 }));
+
+  return (
+    <Toolbar>
+      <Button variant="outlined" onClick={handleCurrent}>
+        This Week
+      </Button>
+      <IconButton onClick={handlePrevious}>
+        <NavigateBeforeIcon />
+      </IconButton>
+      <IconButton onClick={handleNext}>
+        <NavigateNextIcon />
+      </IconButton>
+      <Typography>
+        {start.toLocaleString(DateTime.DATE_MED)}
+      </Typography>
+    </Toolbar>
+  );
+};
+
+export default WeekToolbar;
--- src/pages/Member.tsx
+++ src/pages/Member.tsx
@@ -1,16 +1,12 @@
 import Page from '../components/Page';
+import WeekToolbar from '../components/WeekToolbar';
 
-import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import Fab from '@material-ui/core/Fab';
-import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import AddIcon from '@material-ui/icons/Add';
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import React from 'react';
+import { DateTime } from 'luxon';
+import React, { useState } from 'react';
 
 const useStyles = makeStyles(theme => ({
   fab: {
@@ -21,24 +17,13 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const Member: React.FC = () => {
+  const [week, setWeek] = useState(DateTime.local());
+
   const classes = useStyles();
 
   return (
     <Page title="My Availability">
-      <Toolbar>
-        <Button variant="outlined">
-          Today
-        </Button>
-        <IconButton>
-          <NavigateBeforeIcon />
-        </IconButton>
-        <IconButton>
-          <NavigateNextIcon />
-        </IconButton>
-        <Typography>
-          11th November 2019
-        </Typography>
-      </Toolbar>
+      <WeekToolbar value={week} onChange={week => setWeek(week)} />
       <Divider />
       <Fab color="secondary" className={classes.fab}>
         <AddIcon />
