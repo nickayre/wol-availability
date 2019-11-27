f00101a454dc6d3d6cd80aeed7c6b730e686cab7
Andrew Short <andrewjshort@gmail.com>
2019-11-28 09:31:35 +1100

    Model availabilit

--- src/model/availability.ts
+++ src/model/availability.ts
@@ -1,3 +1,5 @@
+import { Interval } from 'luxon';
+
 export interface Member {
   number: number;
   fullName: string;
@@ -5,4 +7,17 @@ export interface Member {
   team: string;
   permission: string;
   qualifications: string[];
+}
+
+type Available = 'AVAILABLE' | 'UNAVAILABLE';
+type RescueAvailable = 'IMMEDIATE' | 'SUPPORT' | 'UNAVAILABLE';
+
+export interface Availability {
+  interval: Interval;
+
+  storm?: Available;
+  rescue?: RescueAvailable;
+
+  note?: string;
+  vehicle?: string;
 }
