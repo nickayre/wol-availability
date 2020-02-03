15ffbe5f31cfe7a23c8ce7f38f660b4bc090c70f
Andrew Short <andrewjshort@gmail.com>
2020-02-03 15:16:14 +1100

    Availability buttons

--- src/components/AvailabilityForm.tsx
+++ src/components/AvailabilityForm.tsx
@@ -1,8 +1,36 @@
 import { Available, RescueAvailable } from '../model/availability';
 
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form, { FormProps } from 'react-bootstrap/Form';
+import Button, { ButtonProps } from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Form from 'react-bootstrap/Form';
+
+interface RadioButtonGroupProps<T> {
+  options: Array<{ value: T, text: string, variant: string }>;
+  value: T | undefined;
+  onChange: (value: T | undefined) => void;
+}
+
+class RadioButtonGroup<T> extends React.Component<RadioButtonGroupProps<T>, any> {
+  public render() {
+    const { value, options, onChange } = this.props;
+
+    return (
+      <ButtonGroup>
+        {options.map(opt => {
+          const variant = (opt.value === value) ? opt.variant : `outline-${opt.variant}`;
+          const handleClick = () => onChange(opt.value === value ? undefined : opt.value);
+
+          return (
+            <Button variant={variant as ButtonProps['variant']} onClick={handleClick}>
+              {opt.text}
+            </Button>
+          );
+        })}
+      </ButtonGroup>
+    );
+  }
+}
 
 const AvailabilityForm: React.FC = props => {
   const [storm, setStorm] = useState<Available | undefined>();
@@ -14,12 +42,33 @@ const AvailabilityForm: React.FC = props => {
     <Form>
       <Form.Group controlId='storm'>
         <Form.Label>Storm and support</Form.Label>
+        <div>
+          <RadioButtonGroup
+            options={[
+              { value: 'AVAILABLE', text: 'Available', variant: 'success' },
+              { value: 'UNAVAILABLE', text: 'Unavailable', variant: 'danger' },
+            ]}
+            value={storm}
+            onChange={setStorm}
+          />
+        </div>
       </Form.Group>
       <Form.Group controlId='rescue'>
         <Form.Label>Rescue</Form.Label>
+        <div>
+          <RadioButtonGroup
+            options={[
+              { value: 'IMMEDIATE', text: 'Immediate', variant: 'success' },
+              { value: 'SUPPORT', text: 'Support', variant: 'warning' },
+              { value: 'UNAVAILABLE', text: 'Unavailable', variant: 'danger' },
+            ]}
+            value={rescue}
+            onChange={setRescue}
+          />
+        </div>
       </Form.Group>
       <Form.Group controlId='vehicle'>
-        <Form.Label>Vehicle</Form.Label>
+        <Form.Label>Covering vehicle</Form.Label>
         <Form.Control
           type='text'
           value={vehicle}
